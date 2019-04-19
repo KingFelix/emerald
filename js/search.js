2fdd4290ccec9c59b0410a7a54784081e2048f38
shift-return <49747403+shift-return@users.noreply.github.com>
2019-04-19 11:38:25 +0200

    Update search.js

--- js/search.js
+++ js/search.js
@@ -4,6 +4,7 @@ SimpleJekyllSearch({
   searchInput: document.getElementById('search-input'),
   resultsContainer: document.getElementById('results-container'),
   json: '/search.json',
+  searchResultTemplate: '<div><a href="{url}"><h1>{title}</h1></a><span>{date}</span></div>',
   noResultsText ("No result found!") 
 })
 </script>
