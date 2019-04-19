6b89ba3eea508e739555d61d0a68f333b823591c
shift-return <49747403+shift-return@users.noreply.github.com>
2019-04-19 11:32:52 +0200

    Create search.js

---
+++ js/search.js
@@ -0,0 +1,9 @@
+<!-- Configuration -->
+<script>
+SimpleJekyllSearch({
+  searchInput: document.getElementById('search-input'),
+  resultsContainer: document.getElementById('results-container'),
+  json: '/search.json',
+  noResultsText ("No result found!") 
+})
+</script>
